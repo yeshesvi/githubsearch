c9e940a792ae2ac1eeb19ec8da29663fb6f94e45
Yeshesvi Kadiyala <yesh@umich.edu>
2017-11-12 01:12:39 -0500

    Complete transition to bluma.io instead of bootstrap

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -5,17 +5,19 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class GithubService {
   private username: string;
+  private client_id = 'ea496b59e26af186af98';
+  private client_secret = '8db968f47c740c742177c6eeecd743f2ce6c6b16';
   constructor(private http: Http) {
     console.log('github service is ready ...');
     this.username = 'yeshesvi';
   }
   getUser() {
-    return this.http.get('http://api.github.com/users/' + this.username)
+    return this.http.get('http://api.github.com/users/' + this.username + '?client_id=' + this.client_id + '&client_secret=' + this.client_secret)
       .map(res => res.json());
   }
 
   getRepos() {
-    return this.http.get('http://api.github.com/users/' + this.username + '/repos')
+    return this.http.get('http://api.github.com/users/' + this.username + '/repos' + '?client_id=' + this.client_id + '&client_secret=' + this.client_secret)
       .map(res => res.json());
   }
 
